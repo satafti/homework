5ebe523fe6a2a438e027b8e506d4020a39f66aa7
AliReza <taftialireza@gmail.com>
2013-03-03 14:28:42 +0330

    Initial commit

---
+++ Alireza.tf-jq/jq.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("div.box>div").click(function(){
+		$(this).hide();
+	});
+});
---
+++ Alireza.tf-slideshow5/slideshow.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$('div.slideshow div.train > div').css('width',Math.ceil(650/5)+'px');
+};)
